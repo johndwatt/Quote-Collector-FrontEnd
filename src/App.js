a47a6db6eecb96405fbf5fe749248bfed1a51b14
johndwatt <jdw23@protonmail.com>
2022-02-23 17:16:51 -0800

    adds basical navbar and styling

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Quote from "./components/Quote/Quote";
+import Navbar from "./components/Navbar/Navbar";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <Quote />
     </div>
   );
